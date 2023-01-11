9dfdaa00ec50fbf2530ab5560e608e98f8f9c164
guillaume <guillaume.kerherve@scalian.com>
2023-01-11 21:44:19 +0100

    premier commit

---
+++ android/app/src/main/java/fr/meteo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.meteo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.meteo.app',
+  appName: 'app-meteo',
+  webDir: 'dist/app-meteo',
+  bundledWebRuntime: false
+};
+
+export default config;
