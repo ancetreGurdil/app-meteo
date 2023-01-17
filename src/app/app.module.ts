e53faa21ec399d79ea1f831f55a0c818a5915fda
guillaume <guillaume.kerherve@scalian.com>
2023-01-17 21:23:57 +0100

    front - création de la boite principale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from '../@shared/shared.module';
+import { SharedModule } from './@shared/shared.module';
+import { MenuListComponent } from './menu-list/components/menu-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuListComponent
   ],
   imports: [
     SharedModule,
